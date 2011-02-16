84c0e4f5039912ac22d177a8b557af4dd8558c86
Kristijan Sedlak <k.sedlak@mandatela.si>
2011-02-16 22:05:44 +0100

    Now requiring Follow model (fix).

--- lib/acts_as_followable.rb
+++ lib/acts_as_followable.rb
@@ -4,6 +4,7 @@
     require File.join(File.dirname(__FILE__), lib)  
 end
 
-require File.join(File.dirname(__FILE__), '../app/models/follow.rb')
+require File.join(File.dirname(__FILE__), '..', 'app', 'models', 'follow.rb')
+require File.join(Rails.root, 'app', 'models', 'follow.rb') rescue nil
 
 ActiveRecord::Base.send(:include, ActsAsFollowable::Followable)
