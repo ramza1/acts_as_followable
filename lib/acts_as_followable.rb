18f9213df79363b1e2e74ad6275102923776aef8
Kristijan Sedlak <k.sedlak@mandatela.si>
2011-04-07 09:55:09 +0200

    ActsAsFollowable v0.1.7

--- lib/acts_as_followable.rb
+++ lib/acts_as_followable.rb
@@ -4,6 +4,6 @@
 ).each do |lib|
     require File.join(File.dirname(__FILE__), lib)  
 end
-require File.join(Rails.root, 'app', 'models', 'follow') rescue nil
+require File.join(Rails.root||RAILS_ROOT, 'app', 'models', 'follow') rescue nil
 
 ActiveRecord::Base.send(:include, ActsAsFollowable::Followable)
--- lib/acts_as_followable/version.rb
+++ lib/acts_as_followable/version.rb
@@ -2,7 +2,7 @@ module ActsAsFollowable
   module Version
     MAJOR = 0
     MINOR = 1
-    TINY  = 6
+    TINY  = 7
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
