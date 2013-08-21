7909b3b31f9edba6256cfb1e11375605c9400639
uchenna <jin4rill@yahoo.com>
2013-08-21 05:03:46 -0700

    Update follow.rb

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -6,4 +6,5 @@ class Follow < ActiveRecord::Base
   scope :for_followable_type, lambda { |followable_type| where("followable_type = ?", followable_type) }
   scope :for_follower, lambda { |follower| where(["follower_id = ?", follower.id]).for_follower_type(ActsAsFollowable::Lib.class_name(follower)) }
   scope :for_followable, lambda { |followable| where(["followable_id = ?", followable.id]).for_followable_type(ActsAsFollowable::Lib.class_name(followable)) }
+  attr_accessible :followable_id, :followable_type, :follower_id, :follower_type
 end
